5c75d6047964a95b2ff3a86bc0c95b013fb8d561
Simon Tucker <brotherlogic@gmail.com>
2021-12-08 18:39:23 -0800

    Clear error

--- lockutils.go
+++ lockutils.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"time"
-
 	"golang.org/x/net/context"
 )
 
 func (s *Server) runComputation(ctx context.Context) error {
-	t := time.Now()
 	sum := 0
 	for i := 0; i < 10000; i++ {
 		sum += i
